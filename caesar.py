fa3de97faed3e9acd9ebbe39c736b4a2e68f0816
retr00exe <mekelilyasa3@gmail.com>
2020-05-13 05:10:31 +0700

    Menambah Fitur Caesar Cipher

---
+++ caesar.py
@@ -0,0 +1,154 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'caesar.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.2
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_CaesarWindow(object):
+    def setupUi(self, CaesarWindow):
+        CaesarWindow.setObjectName("CaesarWindow")
+        CaesarWindow.resize(850, 585)
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        CaesarWindow.setFont(font)
+        icon = QtGui.QIcon()
+        icon.addPixmap(QtGui.QPixmap("Caesar.jpg"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+        CaesarWindow.setWindowIcon(icon)
+        self.centralwidget = QtWidgets.QWidget(CaesarWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        #LABEL
+        self.labelCaesar = QtWidgets.QLabel(self.centralwidget)
+        self.labelCaesar.setGeometry(QtCore.QRect(20, 10, 191, 41))
+        font = QtGui.QFont()
+        font.setFamily("Times New Roman")
+        font.setPointSize(18)
+        self.labelCaesar.setFont(font)
+        self.labelCaesar.setObjectName("labelCaesar")
+        self.labelShift = QtWidgets.QLabel(self.centralwidget)
+        self.labelShift.setGeometry(QtCore.QRect(20, 60, 51, 41))
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        self.labelShift.setFont(font)
+        self.labelShift.setObjectName("labelShift")
+        self.labelPlainText1 = QtWidgets.QLabel(self.centralwidget)
+        self.labelPlainText1.setGeometry(QtCore.QRect(20, 130, 81, 16))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.labelPlainText1.setFont(font)
+        self.labelPlainText1.setObjectName("labelPlainText1")
+        self.labelCipherText = QtWidgets.QLabel(self.centralwidget)
+        self.labelCipherText.setGeometry(QtCore.QRect(20, 320, 131, 16))
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.labelCipherText.setFont(font)
+        self.labelCipherText.setObjectName("labelCipherText")
+
+
+        self.lineEditShift = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEditShift.setGeometry(QtCore.QRect(80, 70, 81, 22))
+        self.lineEditShift.setObjectName("lineEditShift")
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.lineEditShift.setFont(font)
+
+        self.textEditPlainText1 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEditPlainText1.setGeometry(QtCore.QRect(20, 150, 371, 151))
+        self.textEditPlainText1.setObjectName("textEditPlainText1")
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.textEditPlainText1.setFont(font)
+
+        self.textEditCipherText1 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEditCipherText1.setGeometry(QtCore.QRect(20, 340, 371, 151))
+        self.textEditCipherText1.setObjectName("textEditCipherText1")
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.textEditCipherText1.setFont(font)
+
+        self.textEditPlainText2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEditPlainText2.setGeometry(QtCore.QRect(450, 150, 371, 151))
+        self.textEditPlainText2.setObjectName("textEditPlainText2")
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.textEditPlainText2.setFont(font)
+
+        self.textEditCipherText2 = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEditCipherText2.setGeometry(QtCore.QRect(450, 340, 371, 151))
+        self.textEditCipherText2.setObjectName("textEditCipherText2")
+        font = QtGui.QFont()
+        font.setPointSize(10)
+        self.textEditCipherText2.setFont(font)
+
+        self.pushButtonEncrypt = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButtonEncrypt.setGeometry(QtCore.QRect(20, 510, 93, 28))
+        self.pushButtonEncrypt.setObjectName("pushButtonEncrypt")
+        self.pushButtonEncrypt.clicked.connect(self.encrypt)
+
+        self.pushButtonDecrypt = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButtonDecrypt.setGeometry(QtCore.QRect(130, 510, 93, 28))
+        self.pushButtonDecrypt.setObjectName("pushButtonDecrypt")
+        self.pushButtonEncrypt.clicked.connect(self.decrypt)
+
+        CaesarWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(CaesarWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 420, 23))
+        self.menubar.setObjectName("menubar")
+        CaesarWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(CaesarWindow)
+        self.statusbar.setObjectName("statusbar")
+        CaesarWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(CaesarWindow)
+        QtCore.QMetaObject.connectSlotsByName(CaesarWindow)
+
+    def retranslateUi(self, CaesarWindow):
+        _translate = QtCore.QCoreApplication.translate
+        CaesarWindow.setWindowTitle(_translate("CaesarWindow", "Caesar Cipher"))
+        self.labelCaesar.setText(_translate("CaesarWindow", "Caesar Cipher"))
+        self.labelShift.setText(_translate("CaesarWindow", "Shift :"))
+        self.labelPlainText1.setText(_translate("CaesarWindow", "Input"))
+        self.labelCipherText.setText(_translate("CaesarWindow", "Output"))
+        self.pushButtonEncrypt.setText(_translate("CaesarWindow", "Encrypt"))
+        self.pushButtonDecrypt.setText(_translate("CaesarWindow", "Decrypt"))
+    
+    def encrypt(self): 
+        textboxValue = self.textEditPlainText1.toPlainText()
+        shift = int(self.lineEditShift.text())
+        encrypted = "" 
+        for char in textboxValue:
+            if char == ' ':
+                encrypted = encrypted + char
+            elif  char.isupper():
+                encrypted = encrypted + chr((ord(char) + shift - 65) % 26 + 65)
+            else:
+                encrypted = encrypted + chr((ord(char) + shift - 97) % 26 + 97)
+        self.textEditCipherText1.setText(encrypted)
+
+    def decrypt(self):
+        textboxValue = self.textEditPlainText1.toPlainText()
+        shift = int(self.lineEditShift.text())
+        decrypted = "" 
+        for char in textboxValue:
+            if char == ' ':
+                decrypted = decrypted + char
+            elif  char.isupper():
+                decrypted = decrypted + chr((ord(char) + shift + 65) % 26 + 65)
+            else:
+                decrypted = decrypted + chr((ord(char) + shift + 97) % 26 + 97)
+        self.textEditCipherText1.setText(decrypted)
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    CaesarWindow = QtWidgets.QMainWindow()
+    ui = Ui_CaesarWindow()
+    ui.setupUi(CaesarWindow)
+    CaesarWindow.show()
+    sys.exit(app.exec_())
--- crypt.py
+++ crypt.py
@@ -1,126 +1,121 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, qApp
 from encrypt import *
 from decrypt import *
 
-
 class UI_MainWindow(object):
     listJenis = ["Base64","Base32","Base16","Ascii85","ASCII","Reverse","ROT13","MD5","SHA-1","SHA-224","SHA-256","SHA-384","SHA-512"]
-    def setupUi(self, MainWindow):
+    def setupUI(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(1019, 522)
+        MainWindow.resize(1000, 500)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
 
         self.comboBoxPilihJenis = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBoxPilihJenis.setGeometry(QtCore.QRect(30, 30, 241, 31))
+        self.comboBoxPilihJenis.setGeometry(QtCore.QRect(30, 35, 240, 30))
         self.comboBoxPilihJenis.setObjectName("comboBoxPilihJenis")
         for jenis in self.listJenis:
             self.comboBoxPilihJenis.addItem(jenis)
         inputComboBox = self.comboBoxPilihJenis.currentText()
-        self.comboBoxPilihJenis.currentIndexChanged.connect(self.disable)
+        self.comboBoxPilihJenis.currentIndexChanged.connect(self.disableHash)
         
 #=======================================================================================================================================================#
 #LABEL
+        self.labelPilihAlgoritma = QtWidgets.QLabel(self.centralwidget)
+        self.labelPilihAlgoritma.setGeometry(QtCore.QRect(30, 5, 230, 30))
+        self.labelPilihAlgoritma.setObjectName("labelPilihAlgoritma")
+        
         self.labelPlainText1 = QtWidgets.QLabel(self.centralwidget)
-        self.labelPlainText1.setEnabled(True)
         self.labelPlainText1.setGeometry(QtCore.QRect(30, 80, 55, 20))
         self.labelPlainText1.setObjectName("labelPlainText1")
         
         self.labelPlainText2 = QtWidgets.QLabel(self.centralwidget)
-        self.labelPlainText2.setEnabled(True)
         self.labelPlainText2.setGeometry(QtCore.QRect(530, 290, 55, 20))
         self.labelPlainText2.setObjectName("labelPlainText2")
 
         self.labelEncryptedText1 = QtWidgets.QLabel(self.centralwidget)
-        self.labelEncryptedText1.setGeometry(QtCore.QRect(30, 290, 231, 16))
+        self.labelEncryptedText1.setGeometry(QtCore.QRect(30, 290, 230, 15))
         self.labelEncryptedText1.setObjectName("labelEncryptedText1")
         
         self.labelEncryptedText2 = QtWidgets.QLabel(self.centralwidget)
-        self.labelEncryptedText2.setGeometry(QtCore.QRect(530, 80, 231, 16))
+        self.labelEncryptedText2.setGeometry(QtCore.QRect(530, 80, 230, 15))
         self.labelEncryptedText2.setObjectName("labelEncryptedText2")
         
 #=======================================================================================================================================================#
 #ENCRYPT
         self.textEditPlainText1 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEditPlainText1.setGeometry(QtCore.QRect(30, 110, 391, 111))
+        self.textEditPlainText1.setGeometry(QtCore.QRect(30, 110, 390, 110))
         self.textEditPlainText1.setObjectName("textEditPlainText1")
 
         self.pushButtonEncrypt = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButtonEncrypt.setGeometry(QtCore.QRect(30, 230, 141, 28))
+        self.pushButtonEncrypt.setGeometry(QtCore.QRect(30, 230, 140, 30))
         self.pushButtonEncrypt.setIconSize(QtCore.QSize(20, 20))
         self.pushButtonEncrypt.setObjectName("pushButtonEncrypt")
         self.pushButtonEncrypt.clicked.connect(self.encrypt)
 
         self.textEditEncryptedText1 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEditEncryptedText1.setGeometry(QtCore.QRect(30, 320, 391, 111))
+        self.textEditEncryptedText1.setGeometry(QtCore.QRect(30, 320, 390, 110))
         self.textEditEncryptedText1.setObjectName("textEditEncryptedText1")
 
 #=======================================================================================================================================================#
 #DECRYPT
         self.textEditEncryptedText2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEditEncryptedText2.setGeometry(QtCore.QRect(530, 110, 391, 111))
+        self.textEditEncryptedText2.setGeometry(QtCore.QRect(530, 110, 390, 110))
         self.textEditEncryptedText2.setObjectName("textEditEncryptedText2")
 
         self.pushButtonDecrypt = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButtonDecrypt.setGeometry(QtCore.QRect(530, 230, 141, 28))
+        self.pushButtonDecrypt.setGeometry(QtCore.QRect(530, 230, 140, 30))
         self.pushButtonDecrypt.setIconSize(QtCore.QSize(20, 20))
         self.pushButtonDecrypt.setObjectName("pushButtonDecrypt")
         self.pushButtonDecrypt.clicked.connect(self.decrypt)
 
         self.textEditPlainText2 = QtWidgets.QTextEdit(self.centralwidget)
-        self.textEditPlainText2.setGeometry(QtCore.QRect(530, 320, 391, 111))
+        self.textEditPlainText2.setGeometry(QtCore.QRect(530, 320, 390, 110))
         self.textEditPlainText2.setObjectName("textEditPlainText2")
 
 #=======================================================================================================================================================#
         
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1019, 26))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1020, 25))
         self.menubar.setObjectName("menubar")
-        self.menuEncrpyt = QtWidgets.QMenu(self.menubar)
-        self.menuEncrpyt.setObjectName("menuEncrpyt")
+        self.menu = QtWidgets.QMenu(self.menubar)
+        self.menu.setObjectName("menu")
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
-        self.actionBase64 = QtWidgets.QAction(MainWindow)
-        self.actionBase64.setObjectName("actionBase64")
-        self.actionCaesar_Cipher = QtWidgets.QAction(MainWindow)
-        self.actionCaesar_Cipher.setObjectName("actionCaesar_Cipher")
-        self.actionBase64_2 = QtWidgets.QAction(MainWindow)
-        self.actionBase64_2.setObjectName("actionBase64_2")
-        self.actionBase32 = QtWidgets.QAction(MainWindow)
-        self.actionBase32.setObjectName("actionBase32")
-        self.actionAscii85 = QtWidgets.QAction(MainWindow)
-        self.actionAscii85.setObjectName("actionAscii85")
-        self.actionROT13 = QtWidgets.QAction(MainWindow)
-        self.actionROT13.setObjectName("actionROT13")
-        self.actionMD5 = QtWidgets.QAction(MainWindow)
-        self.actionMD5.setObjectName("actionMD5")
+
         self.actionNew = QtWidgets.QAction(MainWindow)
         self.actionNew.setObjectName("actionNew")
+        self.actionNew.setShortcut("Ctrl+N")
+
         self.actionQuit = QtWidgets.QAction(MainWindow)
         self.actionQuit.setObjectName("actionQuit")
-        self.menuEncrpyt.addAction(self.actionNew)
-        self.menuEncrpyt.addAction(self.actionQuit)
-        self.menubar.addAction(self.menuEncrpyt.menuAction())
-        self.retranslateUi(MainWindow)
+        self.actionQuit.setShortcut("Ctrl+Q")
+        self.actionQuit.triggered.connect(qApp.quit)
+
+        self.menu.addAction(self.actionNew)
+        self.menu.addAction(self.actionQuit)
+        self.menubar.addAction(self.menu.menuAction())
+        self.retranslateUI(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, MainWindow):
+    def retranslateUI(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "Enkripsi dan Dekripsi Kriptografi"))
         MainWindow.setWindowIcon(QtGui.QIcon('icon.jpg'))
         counter=len(self.listJenis)
         for jenis in self.listJenis:
             self.comboBoxPilihJenis.setItemText(counter, _translate("MainWindow",jenis))
+        self.labelPilihAlgoritma.setText(_translate("MainWindow", "Pilih Algoritma"))
         self.labelPlainText1.setText(_translate("MainWindow", "Plain Text"))
         self.labelPlainText2.setText(_translate("MainWindow", "Plain Text"))
-        self.labelEncryptedText1.setText(_translate("MainWindow", "Encrypted Text"))
-        self.labelEncryptedText2.setText(_translate("MainWindow", "Encrypted Text"))
+        self.labelEncryptedText1.setText(_translate("MainWindow", "Ciphertext"))
+        self.labelEncryptedText2.setText(_translate("MainWindow", "Ciphertext"))
         self.pushButtonEncrypt.setText(_translate("MainWindow", "Encrypt"))
         self.pushButtonDecrypt.setText(_translate("MainWindow", "Decrypt"))
-        self.menuEncrpyt.setTitle(_translate("MainWindow", "Menu"))
+        self.menu.setTitle(_translate("MainWindow", "Menu"))
         self.actionNew.setText(_translate("MainWindow", "New"))
         self.actionQuit.setText(_translate("MainWindow", "Quit"))
 
@@ -138,9 +133,10 @@ class UI_MainWindow(object):
         solve.setValue(inputComboBox,textboxValue)
         self.textEditPlainText2.setText(solve.getValue())
 
-    def disable(self):
+    def disableHash(self):
         inputComboBox = self.comboBoxPilihJenis.currentText()
-        if(inputComboBox=="MD5" or inputComboBox=="SHA-1" or inputComboBox=="SHA-224" or inputComboBox=="SHA-256" or inputComboBox=="SHA-384" or inputComboBox=="SHA-512"):
+        hashList = ["MD5","SHA-1","SHA-224","SHA-256","SHA-384","SHA-512"]
+        if(inputComboBox in hashList):
             self.textEditEncryptedText2.setDisabled(True)
             self.textEditPlainText2.setDisabled(True)
         else:
@@ -152,6 +148,6 @@ if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = UI_MainWindow()
-    ui.setupUi(MainWindow)
+    ui.setupUI(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
